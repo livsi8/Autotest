eea5c6ac052e49371cebed9620a44b9237c55da3
Yehor <egorchernyy96@gmail.com>
2020-02-10 10:26:09 +0200

    CW

---
+++ src/main/java/Module_01/Lesson_03/lesson3.java
@@ -0,0 +1,59 @@
+package Module_01.Lesson_03;
+
+public class lesson3 {
+
+    public static void main(String[] args) {
+       // for (int i = 10; i >= 0; i--) {
+            //    if(i % 2 == 0){
+            //        System.out.println();
+            //    }
+
+            //}
+            // вывод чётных
+            //for (int i = 10; i >= 0; i--){
+
+
+            //    if(i % 2 !=0){
+            //        continue;
+            //    }
+            //    System.out.println(i);
+            //}
+            // тернарка
+
+        // int a = 1;
+        // int b = 2;
+        // do {
+        //     System.out.println("Hello");
+        // }
+        // while (a++ <= b);
+        // {
+        // System.out.println("Bye...");
+        //
+        //for (int i = 1; i <= 110; i *=2) {
+        //    System.out.println(i);
+        //}
+
+        // вывести 1 3 6 5 8
+       // for (int i = 0; i <= 110;) {
+
+       //     i = i++ + i+;
+       //     System.out.println(i);
+       // }
+        //  int a = 1;
+        //        int b = 1;
+        //        for (int i = 0; i <= 110; i++) {
+        //            b = a + b;
+        //            b++;
+        //            a++;
+        //            System.out.println(b);
+       //int a = 1;
+       //int b = 1;
+       //int fib = 0;
+       //for (int i = 0; i <= 110; i++) {
+       //    fib = a + b;
+       //    a = b;
+       //    b = fib;
+       //    System.out.println(fib);
+       //}
+    }
+}
