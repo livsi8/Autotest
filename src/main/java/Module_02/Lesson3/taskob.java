68cf9641845259d2718c5b6329be55178e54652b
Yehor <egorchernyy96@gmail.com>
2020-02-26 19:34:46 +0200

    lw

---
+++ src/main/java/Module_02/Lesson3/taskob.java
@@ -0,0 +1,16 @@
+package Module_02.Lesson3;
+
+import java.util.Arrays;
+
+public class taskob {
+
+
+    public static void main(String[] args) {
+
+        String string = "Hello, world!!";
+        String string2 = "Hello, world!";
+        System.out.println(string2);
+        System.out.println(Arrays.toString(string.split(" ")));
+    }
+
+}
