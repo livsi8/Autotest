2a81d6fbdc69cadac7ea260ed17e9bada57e81d5
Yehor <egorchernyy96@gmail.com>
2020-04-25 21:27:24 +0300

    HW

--- src/main/java/ESHOP/MainClass.java
+++ src/main/java/ESHOP/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
         shopAssistant.name = "Alex";
         shopAssistant.age = 25;
         shopAssistant.experienceMonth = 12;
-        shopAssistant.salary = 3000;
+        shopAssistant.salary = 30000;
         shopAssistant.position = "Продавец-консультант";
       //  System.out.println("Должность:" + shopAssistant.position);
       //  System.out.println("Имя:" + shopAssistant.name);
@@ -26,6 +26,18 @@ public class MainClass {
       //  System.out.println("Возраст:" + cashierSeller.age);
       //  System.out.println("Опыт работы:" + cashierSeller.experienceMonth);
       //  System.out.println("Зарплата:" + cashierSeller.salary);
+
+        Staff Security = new Staff();
+        Security.name = "Andrew";
+        Security.age = 19;
+        Security.experienceMonth = 10;
+        Security.salary = 44000;
+        Security.position = "Охранник";
+        // System.out.println("Должность:" + Security.position);
+        // System.out.println("Имя:" + Security.name);
+        // System.out.println("Возраст:" + Security.age);
+        // System.out.println("Опыт работы:" + Security.experienceMonth);
+        // System.out.println("Зарплата:" + Security.salary);
 
     }
 
